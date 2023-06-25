5c1c99ae2a894d2a4afda8c2dc7ee54e8bd49789
satish <satish860@gmail.com>
2023-06-25 15:19:16 +0530

    Download Done

--- src/index.ts
+++ src/index.ts
@@ -15,4 +15,18 @@ app.put('/upload/:key', async (c, next) => {
   return c.text(`Put ${key} successfully!`)
 })
 
+app.get('/download/:key', async (c, next) => {
+  const key = c.req.param('key')
+  if (!key) return c.text('Key is required!', 400)
+  const data = await c.env.MY_BUCKET.get(key)
+  if (!data) return c.text('Not found!', 404)
+  const headers = new Headers();
+  data.writeHttpMetadata(headers);
+  headers.set('etag', data.httpEtag);
+  for (const [name, value] of headers.entries()) {
+    c.header(name, value);
+  }
+  return c.body(data.body)
+})
+
 export default app
